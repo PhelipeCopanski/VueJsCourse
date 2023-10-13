8e64c93d4967eaee4ac3d5fd6579aaa905e1c4f2
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-10-13 16:08:39 -0300

    solução desafio módulo 7

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <app-navigation />
+    <app-header />
+    <app-content />
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    'app-navigation': Navigation,
+    'app-header': Header,
+    'app-content': Content,
+    'app-footer': Footer
+  }
+}
+</script>

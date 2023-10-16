d09251a24a6db9da207ed11ab20e2d2e0403604e
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-10-16 15:47:29 -0300

    acompanhando conteúdo seção 9

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: this.listOfCitacoes
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
